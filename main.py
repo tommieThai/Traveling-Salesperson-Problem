e1e049f73f159e2bff889bdb595d15d5ee1808ec
tommieThai <phucthai2806@gmail.com>
2025-05-05 18:46:34 -0500

    updated main to run dataset

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from utils.tsp_loader import load_tsp_file
 from algorithms.nearest_neighbor import nearest_neighbor
 import matplotlib.pyplot as plt
+import time
 
 
-def plot_tour(coords, tour):
+def plot_tour(coords, tour, title):
     x = [coords[i][0] for i in tour]
     y = [coords[i][1] for i in tour]
     plt.figure(figsize=(10, 6))
     plt.plot(x, y, 'o-')
-    plt.title("TSP Tour - Nearest Neighbor")
+    plt.title(title)
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.grid(True)
@@ -16,15 +17,21 @@ def plot_tour(coords, tour):
 
 
 def main():
-    # Replace with your own .tsp file path
-    filepath = 'data/berlin52.tsp'
-    coords = load_tsp_file(filepath)
+    datasets = ['att48.tsp', 'fri26.tsp', 'gr17.tsp']
 
-    tour, total_distance = nearest_neighbor(coords)
-    print("Tour:", tour)
-    print("Total Distance:", total_distance)
+    for name in datasets:
+        filepath = f'data/{name}'
+        print(f"\nRunning Nearest Neighbor on {name}...")
 
-    plot_tour(coords, tour)
+        coords = load_tsp_file(filepath)
+        start = time.time()
+        tour, total_distance = nearest_neighbor(coords)
+        end = time.time()
+
+        print(f"Total Distance: {total_distance:.2f}")
+        print(f"Execution Time: {end - start:.4f} seconds")
+
+        plot_tour(coords, tour, f"Nearest Neighbor Tour - {name}")
 
 
 if __name__ == "__main__":
