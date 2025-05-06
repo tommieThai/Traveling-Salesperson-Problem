b1421444b58acc475e643ee71874fe28c2c935fe
tommieThai <phucthai2806@gmail.com>
2025-05-05 21:36:24 -0500

    fixed main dataset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.tsp_loader import load_tsp_file
 from algorithms.nearest_neighbor import nearest_neighbor
+from algorithms.simulated_annealing import simulated_annealing
 import matplotlib.pyplot as plt
 import time
 
@@ -17,21 +18,27 @@ def plot_tour(coords, tour, title):
 
 
 def main():
-    datasets = ['att48.tsp', 'lu980.tsp', 'wi29.tsp']
+    datasets = ['att48.tsp', 'wi29.tsp', 'dj38.tsp']
 
     for name in datasets:
         filepath = f'data/{name}'
-        print(f"\nRunning Nearest Neighbor on {name}...")
+        print(f"\nRunning algorithms on {name}...")
 
         coords = load_tsp_file(filepath)
+
+        # Nearest Neighbor
         start = time.time()
-        tour, total_distance = nearest_neighbor(coords)
+        tour_nn, dist_nn = nearest_neighbor(coords)
         end = time.time()
+        print(f"[Nearest Neighbor] Distance: {dist_nn:.2f} | Time: {end - start:.4f}s")
+        plot_tour(coords, tour_nn, f"Nearest Neighbor Tour - {name}")
 
-        print(f"Total Distance: {total_distance:.2f}")
-        print(f"Execution Time: {end - start:.4f} seconds")
-
-        plot_tour(coords, tour, f"Nearest Neighbor Tour - {name}")
+        # Simulated Annealing
+        start = time.time()
+        tour_sa, dist_sa = simulated_annealing(coords)
+        end = time.time()
+        print(f"[Simulated Annealing] Distance: {dist_sa:.2f} | Time: {end - start:.4f}s")
+        plot_tour(coords, tour_sa, f"Simulated Annealing Tour - {name}")
 
 
 if __name__ == "__main__":
